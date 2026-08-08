0ff31c63b8fdb2c0308dab5d5feeebe1a9c15e59
avneeshshrivastava <shrivastavavneesh4122005@gmail.com>
2026-08-09 02:20:20 +0530

    Update models.py

--- zevera/models.py
+++ zevera/models.py
@@ -29,6 +29,17 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
     prod_desc = models.TextField(max_length=500)
     image = models.ImageField(upload_to='product_img/', blank=True, null=True)
+    size = models.IntegerField(blank=True, null=True)
+
+    
+    def __str__(self):
+        return self.name
+
+class extra_info(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='extra_info',null=True)
+    headline = models.CharField(max_length=200)
+    headline_desc = models.CharField(max_length=300)
+
