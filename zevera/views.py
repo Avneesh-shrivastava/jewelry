e0c59e5a3c55da75c550e6742e2c95675dee2676
avneeshshrivastava <shrivastavavneesh4122005@gmail.com>
2026-08-11 19:11:59 +0530

    Update views.py

--- zevera/views.py
+++ zevera/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
 
 def home_page(request):
     categories = Category.objects.all()
@@ -24,13 +27,56 @@ def products(request, id):
 
 def product_overview(request, id):
     product = Product.objects.filter(id=id).first()
-    products = Product.objects.filter(category_id=id)
+    products = Product.objects.all()
     category = Category.objects.get(id=id)
+
+    if request.method == "POST":
+        stars = request.POST.get("rating")
+        review = request.POST.get("review_text")
+
+        
+        Reviews.objects.create(
+            user=request.user,
+            review=review,
+            stars=stars
+        )
+    
     context = {
         "product" : product,
         'category' : category,
         "products" : products,
 
         }
-    print(products.values())
+    
     return render(request, 'product_overview.html',context)
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home_page')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home_page')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home_page')
