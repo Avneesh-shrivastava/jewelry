cb066a83604d6a3940501ea785df238cadb63702
avneeshshrivastava <shrivastavavneesh4122005@gmail.com>
2026-09-07 21:01:34 +0530

    Update urls.py

--- jewelry/urls.py
+++ jewelry/urls.py
@@ -46,5 +46,5 @@ urlpatterns = [
     path('search/', search, name='search'),
     path('profile/', profile, name='profile'),
     path('update-profile/', update_profile, name='update_profile'),
-
+    path('orders/<int:order_id>', update_profile, name='orders'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
