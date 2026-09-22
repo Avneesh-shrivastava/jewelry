857177dfe0d248ffd5274c2d93d4741b3a7e2a61
avneeshshrivastava <shrivastavavneesh4122005@gmail.com>
2026-09-22 12:08:44 +0530

    Update urls.py

--- jewelry/urls.py
+++ jewelry/urls.py
@@ -48,5 +48,7 @@ urlpatterns = [
     path('update-profile/', update_profile, name='update_profile'),
     path('orders/', orders, name='orders'),
     path('newsletter/', newsletter, name='newsletter'),
+    path('forgot-password/', forgot_password, name='forgot_password'),
+    path('reset-password/<uidb64>/<token>/', reset_password, name='reset_password'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
